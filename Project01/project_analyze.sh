3b763515d8b17ae5ae4d819325a48efedeecb7e9
ahujas6 <ahujas6@mcamster.ca>
2019-02-14 18:47:03 +0000

    custom feature encrypt/decrypt added

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -3,6 +3,7 @@ cd ..
 echo "function options"
 echo "press 1 to create merge log"
 echo "press 2 to create todo log"
+echo "press 3 to decrypt or encrypt a file"
 
 read ans
 if [ "$ans" -eq "1" ] ; then
@@ -15,7 +16,28 @@ if [ "$ans" -eq "1" ] ; then
 elif [ "$ans" -eq "2" ] ; then
 	touch ~/CS1XA3/Project01/todo.log
 	grep -r "#TODO" --exclude={todo.log,project_analyze.sh} ~/CS1XA3 >> ~/CS1XA3/Project01/todo.log 
-
+elif [ "$ans" -eq "3" ] ; then
+	echo "press 1 to encrypt"
+	echo "press 2 to decrypt"
+	read a1
+	if [ "$a1" -eq "1" ] ; then
+		echo "enter file name"
+		read file
+		gpg -c "$file"
+		echo "file encrypted"
+		echo "deleting orignal file"
+		rm "$file"
+	elif [ "$a1" -eq "2" ] ; then
+		echo "enter file name"
+                read file
+		gpg --decrypt "$file"
+		echo "putting decryted  text in file"
+		gpg --output "$file" --decrypt "$file"
+		
+		echo "data decryted"
+	else 
+		echo "failed "
+	fi
 else
 	echo "failed"
 fi
